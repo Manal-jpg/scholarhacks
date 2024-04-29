dc46504f78a61f00abede0e921d2af62a328e931
MJ Munkhbayar <78574063+Manal-jpg@users.noreply.github.com>
2024-04-29 16:55:39 -0400

    rebasing

--- src/components/home/mentors.data.ts
+++ src/components/home/mentors.data.ts
@@ -6,8 +6,8 @@ export const data: Array<Mentor> = [
     photo: '/images/mentors/manal.jpeg',
     name: 'Manaljav Munkhbayar',
     category: 'Introduction to Web Development',
-    description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    // description:
+    //   'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     company: {
       name: 'University of Toronto',
       logo: '/images/companies/uoft.png',
@@ -18,8 +18,8 @@ export const data: Array<Mentor> = [
     photo: '/images/mentors/meru.jpg',
     name: 'Ermuun Enkhjavkhlan',
     category: 'Introduction to Programming with Python',
-    description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    // description:
+    //   'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     company: {
       name: 'Google',
       logo: '/images/companies/uoft.png',
@@ -27,38 +27,38 @@ export const data: Array<Mentor> = [
   },
   {
     id: 3,
-    photo: '/images/mentors/noah-buscher-8A7fD6Y5VF8-unsplash.jpg',
+    photo: '/images/mentors/tegel.jpg',
     name: 'Maidar Otgon',
     category: 'Introduction to App Development',
-    description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    // description:
+    //   'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     company: {
       name: 'New York University',
       logo: '/images/companies/nyu.png',
     },
   },
-  {
-    id: 4,
-    photo: '/images/mentors/tegel.jpg',
-    name: 'Tergel Lkhagvasuren',
-    category: 'University Application Process',
-    description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    company: {
-      name: 'Microsoft',
-      logo: '/images/companies/uk.png',
-    },
-  },
-  {
-    id: 5,
-    photo: '/images/mentors/philip-martin-5aGUyCW_PJw-unsplash.jpg',
-    name: 'Tselmeg Altanbayar',
-    category: 'SAT Math Preparation',
-    description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    company: {
-      name: 'Canada',
-      logo: '/images/companies/canada.png',
-    },
-  },
+  // {
+  //   id: 4,
+  //   photo: '/images/mentors/tegel.jpg',
+  //   name: 'Tergel Lkhagvasuren',
+  //   category: 'University Application Process',
+  //   description:
+  //     'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  //   company: {
+  //     name: 'Microsoft',
+  //     logo: '/images/companies/uk.png',
+  //   },
+  // },
+  // {
+  //   id: 5,
+  //   photo: '/images/mentors/philip-martin-5aGUyCW_PJw-unsplash.jpg',
+  //   name: 'Tselmeg Altanbayar',
+  //   category: 'SAT Math Preparation',
+  //   description:
+  //     'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  //   company: {
+  //     name: 'Canada',
+  //     logo: '/images/companies/canada.png',
+  //   },
+  // },
 ]
--- src/components/home/testimonial.data.ts
+++ src/components/home/testimonial.data.ts
@@ -3,62 +3,62 @@ import type { Testimonial } from '@/interfaces/testimonial'
 export const data: Array<Testimonial> = [
   {
     id: 1,
-    title: 'Detailed learning materials',
+    title: 'Education beyond classroom',
     content:
-      'Classes that provide very detailed material in term of making UI UX Design starting team making low and hight quality, system designs, using data layout and make prototypes and testing.',
+      'We believe that education is not just about learning, but also about applying what you have learned in the real world. Our courses are designed to help you learn and apply your knowledge in real-world scenarios.',
     user: {
       id: 1,
-      name: 'Luis Sera',
-      professional: 'UI/UX Engineer',
-      photo: '1.jpg',
+      name: 'MJ Munkhbayar',
+      professional: 'Web Development Instructor',
+      photo: '/public/images/mentors/manal.jpeg',
     },
   },
   {
     id: 2,
-    title: 'Best Quality Online Course!',
+    title: 'Meet like minded students!',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'At ScholarHacks, you will meet students from all over the world who share the same passion for learning as you do. Our community is a great place to make friends and learn from each other.',
     user: {
       id: 1,
-      name: 'Riski',
-      professional: 'Software Engineer',
-      photo: '2.jpg',
+      name: 'Ermuun Enkhjavkhlan',
+      professional: 'Python Course Instructor',
+      photo: '/public/images/mentors/meru.jpg',
     },
-  },
-  {
-    id: 3,
-    title: 'Very complete class',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    user: {
-      id: 1,
-      name: 'Nguyễn Văn',
-      professional: 'FullStack Designer',
-      photo: '3.jpg',
-    },
-  },
-  {
-    id: 4,
-    title: 'Great Quality!',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    user: {
-      id: 1,
-      name: 'Diana Jordan',
-      professional: 'SEO Expert',
-      photo: '4.jpg',
-    },
-  },
-  {
-    id: 5,
-    title: 'Detailed learning materials',
-    content:
-      'Classes that provide very detailed material in term of making UI UX Design starting team making low and hight quality, system designs, using data layout and make prototypes and testing.',
-    user: {
-      id: 1,
-      name: 'Ashley Graham',
-      professional: 'Back-End Developer',
-      photo: '5.jpg',
-    },
+    // },
+    // {
+    //   id: 3,
+    //   title: 'Very complete class',
+    //   content:
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    //   user: {
+    //     id: 1,
+    //     name: 'Nguyễn Văn',
+    //     professional: 'FullStack Designer',
+    //     photo: '3.jpg',
+    //   },
+    // },
+    // {
+    //   id: 4,
+    //   title: 'Great Quality!',
+    //   content:
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    //   user: {
+    //     id: 1,
+    //     name: 'Diana Jordan',
+    //     professional: 'SEO Expert',
+    //     photo: '4.jpg',
+    //   },
+    // },
+    // {
+    //   id: 5,
+    //   title: 'Detailed learning materials',
+    //   content:
+    //     'Classes that provide very detailed material in term of making UI UX Design starting team making low and hight quality, system designs, using data layout and make prototypes and testing.',
+    //   user: {
+    //     id: 1,
+    //     name: 'Ashley Graham',
+    //     professional: 'Back-End Developer',
+    //     photo: '5.jpg',
+    //   },
   },
 ]
